25212635e0f1106be7320689ef0fe889d1f01e81
nekiro <nekiroboy@gmail.com>
2020-12-28 13:42:42 +0100

    Add meetings controller

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
-                   Welcome to VetBooking.
+                   Meetings.
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/meetings', [App\Http\Controllers\MeetingsController::class, 'index'])->name('meetings');
