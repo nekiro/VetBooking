0ea9b4ec32484ed1da767c4cb2993d9610aff776
asq95 <kaczorr95@gmail.com>
2020-12-28 23:20:09 +0100

    Add meeting view, better login page
    Add meeting view, better login page

--- resources/views/meetings.blade.php
+++ resources/views/meetings.blade.php
@@ -1,15 +1,75 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                   Meetings.
+<div class="container" id="meetings">
+    <ul id="myTab" class="nav nav-tabs row">
+        <li class="nav-item">
+            <a href="#home" class="nav-link active" data-toggle="tab">Umów się</a>
+        </li>
+        <li class="nav-item">
+            <a href="#profile" class="nav-link" data-toggle="tab">Aktualne wizyty</a>
+        </li>
+    </ul>
+    <div class="tab-content">
+        <div class="tab-pane fade show active p-3" id="home">
+            <h4 class="mt-2 mb-4">Umów się na wizytę</h4>
+            <form action="/home/Create" method="post">
+                <div class="form-group">
+                    <label for="name">Temat wizyty</label>
+                    <input type="text" class="form-control" id="name" name="Name" aria-describedby="name"
+                        placeholder="Temat wizyty">
+                    <h6 id="namecheck" style="color: red; ">Musisz podać temat wizyty</h6>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="description">Opis <br /> <span class="small">Proszę podać gatunek, rasę, wiek zwierzęcia
+                            oraz w skrócie opisać czego będzie dotyczyła wizyta.</span></label>
+                    <textarea class="form-control rounded-0" name="Description" id="description" rows="4"
+                        placeholder="Opis" maxlength="2000"></textarea>
+                    <h6 id="descriptioncheck" style="color: red;">Musisz podać opis wizyty</h6>
+                </div>
+                <h6 class="mb-4">Wybierz termin wizyty.</h6>
+
+                <input id="datetimepicker" name="Date" type="text" placeholder="wybierz datę">
+                <input id="timepicker" name="Time" type="text" placeholder="wybierz godzinę" disabled>
+                <h6 id="pickercheck" style="color: red;">Musisz podać date/godzine wizyty.</h6>
+                </br>
+                <button id="submitBtn" type="submit" class=" mt-4 btn btn-primary centerButton">Zatwierdź</button>
+            </form>
+        </div>
+        <div class="tab-pane fade" id="profile">
+            <h4 class="mt-2">Aktualne wizyty</h4>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nazwa</th>
+                        <th>Opis</th>
+                        <th>Data</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr>
+                        <td>
+                            nazwa
+                        </td>
+                        <td>
+                            opis
+                        </td>
+                        <td>
+                            data
+                        </td>
+                        <td>
+                            <a style="color: blue; text-decoration: underline; cursor: pointer; ">Odwołaj</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
+    <hr>
+
+</div>
+
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,96 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                   Welcome to VetBooking.
-                </div>
+    @guest
+    <div class="row center loginOrRegister">
+        <div class="col-sm">
+
+            <div class="login">
+                <h2 class="login_head text-center">Zaloguj się</h2>
+                <p class="login_p text-center">Jeżeli posiadasz już konto - zaloguj się!</p>
+
+
+
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class=" login_input input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="background: transparent"><svg
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00a3e8"
+                                    class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path
+                                        d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
+                                    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                    <path fill-rule="evenodd"
+                                        d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z" />
+                                </svg></div>
+                        </div>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" placeholder="{{                   __('E-Mail Address') }}"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class=" login_input input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" style="background: transparent"><svg
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#00a3e8"
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M10 16c0-1.104.896-2 2-2s2 .896 2 2c0 .738-.404 1.376-1 1.723v2.277h-2v-2.277c-.596-.347-1-.985-1-1.723zm11-6v14h-18v-14h3v-4c0-3.313 2.687-6 6-6s6 2.687 6 6v4h3zm-13 0h8v-4c0-2.206-1.795-4-4-4s-4 1.794-4 4v4zm11 2h-14v10h14v-10z" />
+                                </svg></div>
+                        </div>
+                        <input id="password" type="password"
+                            class="login_input form-control @error('password') is-invalid @enderror"
+                            placeholder="{{ __('Password') }}" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class=" login_check form-check text-center">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                            {{ old('remember') ? 'checked' : '' }}>
+
+                        <label class="form-check-label" for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
+
+                    <div class="form-group row mb-0 text-center">
+                        <button type="submit" class="btn btn-primary login_btn">
+                            {{ __('Login') }}
+                        </button>
+                    </div>
+
+                </form>
+
+
+                @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    Zapomniałeś hasła?
+                </a>
+                @endif
+                <hr>
+                @if (Route::has('register'))
+                <p class="login_p"> Nie masz konta? <a class="nav-link" style="display: inline-block"
+                        href="{{ route('register') }}">Zarejestruj się!</a> </p>
+
+
+                @endif
             </div>
         </div>
     </div>
+    @endguest
+
 </div>
 @endsection
