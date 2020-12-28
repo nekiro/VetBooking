3dc0ba453e1acfc55943f75da776a29b41f4286c
nekiro <nekiroboy@gmail.com>
2020-12-28 12:50:45 +0100

    Add laravel version

--- VetBooking/Services/MailSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VetBooking.Services
-{
-    public class MailSettings
-    {
-        public string Mail { get; set; }
-        public string Password { get; set; }
-        public string Host { get; set; }
-    }
-}
