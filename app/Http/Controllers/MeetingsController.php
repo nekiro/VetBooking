04de53c0a738ecce2c6ce4ca1e8de5dd08ee7ce3
nekiro <nekiroboy@gmail.com>
2021-01-02 15:23:47 +0100

    Require email verification

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -16,7 +16,7 @@ class MeetingsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('verified');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('auth/login');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
