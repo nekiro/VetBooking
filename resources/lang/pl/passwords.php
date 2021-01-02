351ce4ce3126ad87b39eab9b52105199e8243cfa
nekiro <nekiroboy@gmail.com>
2021-01-02 18:06:36 +0100

    Add polish language translation

---
+++ resources/lang/pl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Twoje hasło zostało zresetowane.',
+    'sent' => 'Wysłaliśmy link do zresetowania hasła, sprawdź swoją skrzynkę pocztową.',
+    'throttled' => 'Poczekaj chwile przed następną próbą.',
+    'token' => 'Token zreestowania hasła jest nieprawidłowy.',
+    'user' => "Użytkownik z tym adresem email nie istnieje.",
+
+];
