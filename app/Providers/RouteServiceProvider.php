9bbc60afecc28ce1e436c7d029863f89c8a9e6a2
asq95 <kaczorr95@gmail.com>
2021-01-09 16:38:33 +0100

    change home
    change home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/meetings/book-meeting';
 
     /**
      * The controller namespace for the application.
